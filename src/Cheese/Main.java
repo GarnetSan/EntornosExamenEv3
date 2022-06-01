dc7b2de09b07e7d33224024ff4679b47a2dfc271
RogueGarnet <michelsonrt@gmail.com>
2022-06-01 11:34:08 +0200

    COMMIT main MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -15,5 +15,19 @@ public class Main {
      public static void main(String[] args) {
     
          System.out.println("1º Gdam: Entornos de desarrollo");
+         String cod1 = "001";
+         Producto prod1 = new Producto(cod1, "Entera", 1, 1.2);
+         
+         String cod2 = "002";
+         Producto prod2 = new Producto(cod2, "Semi", 3, 1.1);
+         
+         
+         ListaProductos miListaProductos = new ListaProductos();
+         
+         miListaProductos.addProducto(prod1);
+         miListaProductos.buscarProducto(cod1).imprime_etiqueta();
+         miListaProductos.addProducto(prod2);
+         miListaProductos.buscarProducto(cod2).imprime_etiqueta();
+         
     }
 }
